b90a3c86136cc6bd3ce664976c45b78644c2263f
felixthibaultgiguere <felix.thibault-giguere@usherbrooke.ca>
2018-11-15 10:01:51 -0500

    inclusion librairie_bras fonctionnel

--- RobotChef/lib/librairie_bras/src/librairie_bras.h
+++ RobotChef/lib/librairie_bras/src/librairie_bras.h
@@ -10,7 +10,10 @@
  */
 
 //--- LISTE DES PROTOTYPES ---//
+#ifndef LIBRAIRIE_BRAS_H
+#define LIBRAIRIE_BRAS_H
 
+#include<LibRobus.h>
 
 /*
  * @Nom : 
@@ -28,3 +31,31 @@ void flip_bras(bool p);
  * @Sortie : 
  */
 void pince(bool p);
+
+void flip_bras(bool p)
+{
+    SERVO_Enable(0);
+    if(p)
+    {
+        SERVO_SetAngle(0,0);
+    }
+    else
+    {
+        SERVO_SetAngle(0,180);
+    }   
+}
+void pince(bool p)
+{
+    SERVO_Enable(1);
+    if(p)
+    {
+      SERVO_SetAngle(1,90);  
+    }
+    else
+    {
+        SERVO_SetAngle(1,0);
+    }
+    
+}
+
+#endif
